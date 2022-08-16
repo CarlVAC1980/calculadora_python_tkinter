e11b16828acba62f74137bd15632a528e2ef3e08
Carlos M Valenca <cavac10@gmail.com>
2022-08-15 22:01:58 -0300

    logica 1:09:34

--- calculator_class.py
+++ calculator_class.py
@@ -1,3 +1,4 @@
+import re
 import tkinter as tk
 from typing import List
 
@@ -7,12 +8,55 @@ class Calculator:
             root: tk.Tk,
             label: tk.Label,
             display: tk.Entry,
-            button: List[List[tk.Button]]
+            buttons: List[List[tk.Button]]
     ):
         self.root = root
         self.label = label
         self.display = display
-        self.button = button
+        self.buttons = buttons
 
     def start(self):
+        self._config_buttons()
+        self._config_display()
         self.root.mainloop()
+
+    def _config_buttons(self):
+        buttons = self.buttons
+        for row_values in buttons:
+            for button in row_values:
+                button_text = button['text']
+
+                if button_text == 'C':
+                    button.bind('<Button-1>', self.clear)
+
+                if button_text in '0123456789.+-/*()^':
+                    button.bind('<Button-1>', self.add_text_to_display)
+
+                if button_text == '=':
+                    button.bind('<Button-1>', self.calculate)
+
+    def _config_display(self):
+        ...
+
+    def _fix_text(self, text):
+        # Substitui tudo que não for 0123456789./*-+^para nada
+        text = re.sub(r'[^\d\.\/\*\-\+\^e]', r'', text, 0)
+        # Substitui sinais repetidos para apenas um sinal
+        text = re.sub(r'([\.\/\*\-\+\^])\1+', r'\1', text, 0)
+        # Substitui () ou *() para nada
+        text = re.sub(r'\*?\(\)', '', text)
+
+        return text
+
+    def clear(self, event=None):
+        self.display.delete(0, 'end')
+
+    def add_text_to_display(self, event=None):
+        self.display.insert('end', event.widget['text'])
+
+    def calculate(self, event=None):
+        fixed_text = self._fix_text(self.display.get())
+        equations = self._get_equations(fixed_text)
+
+    def _get_equations(selfself, text):
+        return re.split(r'\^', text, 0)
